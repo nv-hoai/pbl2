81f076949735dae86d912d798392305b20c9f54a
Duc Duy <h.ducduy2k5@gmail.com>
2024-10-15 07:50:24 +0700

    VERSION 1.3

--- include/service.h
+++ include/service.h
@@ -7,6 +7,7 @@ using namespace std;
 
 class service{
     private:
+        static int service_cout;
         int service_id;
         int customer_id;
         string service_type;
@@ -15,7 +16,7 @@ class service{
         int total_cost;
     public:
         service();
-        service(int a, int b, string x, int y, string c, int d);
+        service( int b, string x, int y, string c);
         service(const service&f);
         void change_cost( int newcost);
         void change_name( string newname);
